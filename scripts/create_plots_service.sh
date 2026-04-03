bb152b7c89853371907a707932b71d185ea9427d
Brian K <bkuschak@yahoo.com>
2026-04-03 16:13:08 -0700

    Kill the plotting script if it hangs.

--- scripts/create_plots_service.sh
+++ scripts/create_plots_service.sh
@@ -10,7 +10,15 @@ cd "$(dirname "$0")"
 # Run at 10 minute intervals.
 while true; do
     echo "===================================================================="
-    ./create_plots.sh
+
+    # If for some reason the script stalls, kill it after 2 hours so it
+    # doesn't hang our loop.
+    /usr/bin/timeout -k30s 2h ./create_plots.sh
+    RET=$?
+    if [ "${RET}" == "124" ]; then
+        echo "Timeout! Killed create_plots.sh due to excessive runtime."
+    fi
+
     echo "Sleeping until next time..."
     sleep 600
 done
